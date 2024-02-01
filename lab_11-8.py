80540673795e1204e3ca98e740d54743d50b5e6c
dillanvaldes1 <144148099+dillanvaldes1@users.noreply.github.com>
2024-02-01 09:03:38 -0500

    Add files via upload

---
+++ lab_11-8.py
@@ -0,0 +1,17 @@
+print("length_multiples")
+list = ["red, green, yellow, blue"]
+
+for i, v in enumerate(list):
+    print (i,v)
+
+    print("length_multiples")
+list = ["2, 4, 6, 8"]
+
+for i, v in enumerate(list):
+    print (i,v)
+
+    print("length_multiples")
+list = ["1, 1.5, 2, 2.5"]
+
+for i, v in enumerate(list):
+    print (i,v)
